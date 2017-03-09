8c7c4cb72e00f53bde72349af42b61f23817ab43
Evan Hickman <travisevanhickman@gmail.com>
2017-03-09 14:42:43 -0600

    setup project

--- app/index.js
+++ app/index.js
@@ -1,2 +1,24 @@
 import 'whatwg-fetch';
 import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import App from './routes/application.vue';
+import Index from './routes/index.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    name: 'index',
+    component: Index,
+  },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  routes,
+});
+
+const app = new Vue({ ...App, router });
+app.$mount('.app');
