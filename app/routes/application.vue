f19484e0c4b3b4f5ea5214149baca196cf409d5d
Evan Hickman <travisevanhickman@gmail.com>
2017-03-10 10:49:57 -0600

    style sidebar

--- app/routes/application.vue
+++ app/routes/application.vue
@@ -5,32 +5,144 @@
         <a href="#" class="nav-item">Puppies</a>
       </div>
       <div class="nav-right">
-        <a href="#" class="nav-item">
-          <p>All Puppies</p>
-        </a>
-        <a href="#" class="nav-item">
-          <p>Add Puppy</p>
-        </a>
+        <router-link :to="{ name: 'index' }" class="nav-item">All Puppies</router-link>
+        <router-link :to="{ name: 'new' }" class="nav-item">Add Puppy</router-link>
       </div>
     </nav>
+    <div class="section">
+      <div class="container">
+        <div class="columns">
+          <div class="column is-one-third">
+            <nav class="panel">
+              <p class="panel-heading">
+                Adopt a Friend
+              </p>
+              <div class="panel-block">
+                <article class="media">
+                  <figure class="media-left">
+                    <p class="image">
+                      <img src="http://bulma.io/images/placeholders/64x64.png">
+                    </p>
+                  </figure>
+                  <div class="media-content">
+                    <div class="content">
+                      <div>Geraldine Beane</div>
+                      <!-- TODO: Update this to add id params -->
+                      <a :to="{ name: 'detail' }"><small>More Info</small></a>
+                    </div>
+                  </div>
+                </article>
+              </div>
+              <div class="panel-block">
+                <article class="media">
+                  <figure class="media-left">
+                    <p class="image">
+                      <img src="http://bulma.io/images/placeholders/64x64.png">
+                    </p>
+                  </figure>
+                  <div class="media-content">
+                    <div class="content">
+                      <div>Geraldine Beane</div>
+                      <!-- TODO: Update this to add id params -->
+                      <a :to="{ name: 'detail' }"><small>More Info</small></a>
+                    </div>
+                  </div>
+                </article>
+              </div>
+              <div class="panel-block">
+                <article class="media">
+                  <figure class="media-left">
+                    <p class="image">
+                      <img src="http://bulma.io/images/placeholders/64x64.png">
+                    </p>
+                  </figure>
+                  <div class="media-content">
+                    <div class="content">
+                      <div>Geraldine Beane</div>
+                      <!-- TODO: Update this to add id params -->
+                      <a :to="{ name: 'detail' }"><small>More Info</small></a>
+                    </div>
+                  </div>
+                </article>
+              </div>
+              <div class="panel-block">
+                <article class="media">
+                  <figure class="media-left">
+                    <p class="image">
+                      <img src="http://bulma.io/images/placeholders/64x64.png">
+                    </p>
+                  </figure>
+                  <div class="media-content">
+                    <div class="content">
+                      <div>Geraldine Beane</div>
+                      <!-- TODO: Update this to add id params -->
+                      <a :to="{ name: 'detail' }"><small>More Info</small></a>
+                    </div>
+                  </div>
+                </article>
+              </div>
+              <div class="panel-block">
+                <article class="media">
+                  <figure class="media-left">
+                    <p class="image">
+                      <img src="http://bulma.io/images/placeholders/64x64.png">
+                    </p>
+                  </figure>
+                  <div class="media-content">
+                    <div class="content">
+                      <div>Geraldine Beane</div>
+                      <!-- TODO: Update this to add id params -->
+                      <a :to="{ name: 'detail' }"><small>More Info</small></a>
+                    </div>
+                  </div>
+                </article>
+              </div>
+              <div class="panel-block">
+                <article class="media">
+                  <figure class="media-left">
+                    <p class="image">
+                      <img src="http://bulma.io/images/placeholders/64x64.png">
+                    </p>
+                  </figure>
+                  <div class="media-content">
+                    <div class="content">
+                      <div>Geraldine Beane</div>
+                      <!-- TODO: Update this to add id params -->
+                      <a :to="{ name: 'detail' }"><small>More Info</small></a>
+                    </div>
+                  </div>
+                </article>
+              </div>
+              </div>
+              <div class="column is-two-thirds">
+                <router-view></router-view>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import store from '../store';
-import { findAll } from '../actions/puppy';
+import {
+  findAll
+} from '../actions/puppy';
 
 
 export default {
+  name: 'Application',
   data() {
     return {
       puppies: this.$select('puppies'),
     };
   },
 
-  // created() {
-  //   store.dispatch(findAll());
-  // },
+  created() {
+    store.dispatch(findAll());
+  },
 
   methods: {
 
